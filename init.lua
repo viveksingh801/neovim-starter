66ab30fbd1f1ead127d40549f7bf3f1b2a4cee41
Vivek <vivek.singh@accolitedigital.com>
2024-04-12 12:43:43 +0530

    2 git plugins for inline git stuff (Gitsupercharger) and in buf git commands

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -15,6 +14,3 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
-
-
-
