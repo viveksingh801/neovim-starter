2054c3071e90b46d03bdddf06c467c5bca02ea25
Vivek <vivek.singh@accolitedigital.com>
2024-04-17 17:38:45 +0530

    Few more plugins

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,4 +3,14 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set number")
 vim.cmd("set shiftwidth=2")
+-- To set the default vim default file structure in tree mode
+vim.cmd("let g:netrw_liststyle = 3")
 vim.g.mapleader = " "
+
+local opt = vim.opt 
+
+opt.relativenumber = true
+opt.number = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.cursorline= true
